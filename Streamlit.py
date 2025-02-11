9cdca7c5aa1e33f6afadfe1dc2b0418fad5d7270
Baze-Bai <15583405928@163.com>
2025-02-10 23:57:05 -0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -25,8 +25,8 @@ aws_region = "us-east-1"
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY,
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"),
     region_name=aws_region
 )
 
